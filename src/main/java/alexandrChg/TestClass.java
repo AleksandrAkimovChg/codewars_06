3bb40fb08c3a4742fc230fac64a09ac4174b02f0
Aleksandr Akimov <gtwood@yandex.ru>
2023-03-18 01:28:36 +0300

    test commit

---
+++ src/main/java/alexandrChg/TestClass.java
@@ -0,0 +1,20 @@
+package alexandrChg;
+
+public class TestClass {
+
+//    int salarySum;
+//    public int getSum(Double[] employeeArray) {
+//
+//        if(employeeArray.length == 0) {
+//
+//            return -1;
+//        }
+//
+//        int sum = 0;
+//        for(int i = 0; i < employeeArray.length; i++) {
+//            salarySum += employeeArray[i];
+//        }
+//
+//        return salarySum;
+//    }
+}
